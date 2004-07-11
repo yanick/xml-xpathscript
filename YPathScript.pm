3473b591511ec3a82ba8f149b62f3384948dbe76
yanick <yanick>
2004-07-11 21:03:56 +0000

    Added svn tags

--- YPathScript.pm
+++ YPathScript.pm
@@ -215,6 +215,8 @@ the matching templates (as hash references).
 =cut "
 
 package XML::YPathScript;
+# Revision: $Rev: 12 $
+# Last Changed: $Date: 2004-07-11 16:37:16 -0400 (Sun, 11 Jul 2004) $   
 
 use vars qw( $VERSION $XML_parser $DoNotInterpolate );
 
