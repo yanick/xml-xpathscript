5112cd0e0abfc30a1656f838fa1bbe369e70c658
yanick <yanick>
2007-06-07 04:44:38 +0000

    Perlcritic changes

--- lib/XML/XPathScript/Processor/LibXML.pm
+++ lib/XML/XPathScript/Processor/LibXML.pm
@@ -1,36 +1,36 @@
+package XML::XPathScript::Processor::LibXML;
+
 use strict;
 use warnings;
-
-package XML::XPathScript::Processor::LibXML;
 
 use base qw/ XML::XPathScript::Processor /;
 
 our $VERSION = '1.49';
 
-sub get_namespace { 
+sub get_namespace {
         my $ns = $_[1]->getNamespaces();
         return $ns ? $ns->getData() : () ;
 }
 
-sub is_text_node { 
-	# little catch: XML::LibXML::Comment is a
-	# XML::LibXML::Text
-        $_[1]->isa( 'XML::LibXML::Text' ) 
-    && !$_[1]->isa( 'XML::LibXML::Comment' );
+sub is_text_node {
+    # little catch: XML::LibXML::Comment is a
+    # XML::LibXML::Text
+    return     $_[1]->isa('XML::LibXML::Text')
+           && !$_[1]->isa('XML::LibXML::Comment');
 }
 
-sub get_attributes   { $_[1]->attributes }
-sub get_text_content { $_[1]->textContent }
-sub get_child_nodes  { $_[1]->childNodes }
-sub get_node_name    { $_[1]->localname }
-sub is_element_node  { $_[1]->isa( 'XML::LibXML::Element' ); }
-sub is_comment_node  { $_[1]->isa( 'XML::LibXML::Comment' ); }
-sub is_pi_node       { $_[1]->isa( "XML::LibXML::PI" ); }
-sub is_nodelist      { $_[1]->isa( 'XML::LibXML::NodeList' ); }
+sub get_attributes   { return $_[1]->attributes }
+sub get_text_content { return $_[1]->textContent }
+sub get_child_nodes  { return $_[1]->childNodes }
+sub get_node_name    { return $_[1]->localname }
+sub is_element_node  { return $_[1]->isa( 'XML::LibXML::Element' ); }
+sub is_comment_node  { return $_[1]->isa( 'XML::LibXML::Comment' ); }
+sub is_pi_node       { return $_[1]->isa( 'XML::LibXML::PI' ); }
+sub is_nodelist      { return $_[1]->isa( 'XML::LibXML::NodeList' ); }
 
 sub get_attribute {
     return $_[1]->isa( 'XML::LibXML::Namespace' )
-         ?  ' xmlns:'.$_[1]->getName() . '="' . $_[1]->value() . '" '
+         ?  ' xmlns:' . $_[1]->getName() . q{="} . $_[1]->value() . q{" }
          : $_[1]->toString( 0, 1 )
          ;
 }
