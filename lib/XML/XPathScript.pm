d35e67780e8b2a7975bb25a0629c37ed454a9e06
yanick <yanick>
2007-07-06 12:37:51 +0000

    added Clone dependency + tagged for 1.52

--- lib/XML/XPathScript.pm
+++ lib/XML/XPathScript.pm
@@ -239,7 +239,7 @@ use File::Basename;
 use XML::XPathScript::Processor;
 use XML::XPathScript::Template;
 
-our $VERSION = '1.51';
+our $VERSION = '1.52';
 
 $XML_parser = 'XML::LibXML';
 
--- lib/XML/XPathScript/Template.pm
+++ lib/XML/XPathScript/Template.pm
@@ -13,7 +13,7 @@ use Scalar::Util qw/ refaddr /;
 use overload '&{}'  => \&_overload_func,
              q{""}  => \&_overload_quote;
 
-our $VERSION = '1.51';
+our $VERSION = '1.52';
 
 sub new {
    my( $class ) = @_;
